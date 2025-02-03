764922a42346b45dfbe58895e2a11757b09e7d89
christadejesus <christa.tech@outlook.com>
2025-02-02 20:16:03 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,104 @@
+# Tomato Timer
+
+[Features](#features) | [Usage](#how-to-use) | [Customization](#for-developers-or-customization) | [Attributions](#attributions) | [Contact](#contact)
+
+Tomato Timer is a user-friendly and enhanced version of a time-management tool inspired by the Pomodoro Technique. Originally a course project from the [100 Days of Code: The Complete Python Pro Bootcamp](https://www.udemy.com/course/100-days-of-code/) by Dr. Angela Yu, this version includes several custom changes and additional features for a more engaging experience.
+
+Built with Python and Tkinter, Tomato Timer is packaged using ```PyInstaller```, allowing it to run conveniently as a standalone program on your desktop.
+
+## Features
+
+- **Work and Break Cycles:** Tomato Timer follows the basic structure of the Pomodoro technique with 25-minute work sessions, 5-minute short breaks, and a 20-minute long break after every 4 work sessions.
+
+- **Visual Timer:** A dynamic display shows the time remaining during work or break sessions with large, clear numbers for easy tracking.
+
+- **Session Count:** After each completed work session, a ✔ mark is added to track your progress.
+
+### Enhanced Features
+
+- **Audio Alerts:** Notification sounds play at the start of each work or break session (requires ```Pygame``` for sound functionality).
+- **Enhanced Button Control:** The 'Start' button is disabled once clicked, preventing multiple clicks and ensuring the timer runs smoothly.
+- **Styled Buttons:** Added support for ```ttk``` to introduce visually appealing, styled buttons with hover effects.
+- **Custom Styling:** The user interface has been visually enhanced with unique fonts and vibrant colors to create a user-friendly experience.
+- **Custom Graphics:** Designed a custom tomato graphic and icon to replace the default image, making the display more playful and engaging.
+- **Convenient Packaging:** Packaged the program using ```PyInstaller``` to make it easy to launch directly from a desktop shortcut, without needing to manually run the Python script.
+
+## How to Use
+### Running the Program
+**1. Download the Executable:**
+
+- Simply download the ```tomato_timer.exe``` file from the [Releases](#releases) section of the repository.
+
+**2. Running the Timer:**
+
+- Double-click the ```tomato_timer.exe``` file to launch the program from your desktop or file manager.
+
+- The timer interface will open, and you can start using it immediately.
+
+**3. Session Control:**
+
+- Press the 'Start' button to begin your first work session.
+- The timer will automatically switch between work and break sessions with sound notifications to keep you on track.
+
+**4. Finishing a Set:**
+
+- Following your final 20 minute break, the timer will stop. 
+- Press the 'Reset' button to immediately begin a new set of sessions.
+
+**5. Exiting the Program:**
+
+- Close the window at any time to stop the timer and exit the program. 
+
+**Note:** If you would like to add a custom tomato icon to your desktop shortcut, feel free to download the ```tomato.ico``` file from the ```images``` folder of this repo.
+
+## For Developers or Customization:
+
+**1. Clone the Repository:**
+
+- If you'd like to modify the code or customize the timer, clone the repository from GitHub:
+
+```bash
+git clone https://github.com/christadejesus/tomato-timer.git
+```
+**2. Install Dependencies:**
+
+- Create and activate a virtual environment, then install the necessary dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+**3. Run the Program:**
+
+- You can run the program by executing the ```tomato_timer.py``` file in your terminal:
+
+```bash
+python tomato_timer.py
+```
+**4. Modifications:**
+
+Feel free to modify the Python code, images, or sounds. Once you've made changes, you can repackage the program using ```PyInstaller``` (see the [Attributions](#attributions) section).
+
+
+## Attributions
+
+- **Sound Effects**: [floraphonic](https://pixabay.com/users/floraphonic-38928062) from [Pixabay](https://pixabay.com/sound-effects)
+- **Project Inspiration**: [100 Days of Code: The Complete Python Pro Bootcamp](https://www.udemy.com/course/100-days-of-code/), Dr. Angela Yu
+- **Pygame** 
+  - [GitHub repo](https://github.com/pygame/pygame)
+  -  License: [GNU LGPL version 2.1](https://www.gnu.org/licenses/lgpl-3.0.html)
+- **PyInstaller**
+  - [GitHub repo](https://github.com/pyinstaller/pyinstaller)
+  - License: [GNU GPL](https://gnu.org/licenses/gpl-2.0.html)
+- **Google Fonts**
+  - [Pacifico](https://fonts.google.com/specimen/Pacifico)
+  - [Poppins](https://fonts.google.com/Poppins)
+  
+## Get in Touch
+
+If you have any questions, suggestions, or want to contribute to this project, feel free to reach out:
+
+Email: christa.tech@outlook.com
+
+LinkedIn: [@christatech](https://www.linkedin.com/in/christatech)
+
+GitHub: [github.com/christadejesus](https://github.com/christadejesus)
---
+++ tomato_timer.py
@@ -0,0 +1,147 @@
+import os
+import sys
+from tkinter import *
+from tkinter import ttk
+import tkinter.font as tkFont
+import math
+import pygame
+
+window = Tk()
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for both dev and PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except AttributeError:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+# ---------------------------- SOUND ------------------------------- #
+pygame.mixer.init()
+break_sound = pygame.mixer.Sound(resource_path("sound/hotel-bell-ding-3-202587.wav"))
+work_sound = pygame.mixer.Sound(resource_path("sound/hotel-bell-ding-1-174457.wav"))
+
+# ---------------------------- IMAGES ------------------------------- #
+icon_image = PhotoImage(file=resource_path("images/tomato_icon.png"))
+tomato_img = PhotoImage(file=resource_path("images/tomato.png"))
+
+# ---------------------------- FONTS ------------------------------- #
+font_path = "fonts/Pacifico-Regular.ttf"
+window.tk.call('font', 'create', 'Pacifico', '-family', 'Pacifico')
+window.tk.call('font', 'create', 'Poppins', '-family', 'Poppins')
+PACIFICO_FONT_LG = tkFont.Font(family="Pacifico", size=32, weight="bold")
+PACIFICO_FONT_SM = tkFont.Font(family="Pacifico", size=14, weight="bold")
+POPPINS_FONT = tkFont.Font(family="Poppins", size=12)
+
+# ---------------------------- COLOR CONSTANTS ------------------------------- #
+PINK = "#f13f48"
+RED = "#df2e38"
+GREEN = "#529e4c"
+DARK_GREEN = "#286324"
+YELLOW = "#ffeeaa"
+WHITE = "#ffffff"
+
+# ---------------------------- TIME CONSTANTS ------------------------------- #
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- COUNTER VARIABLES ------------------------------- #
+reps = 0
+timer = None
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    start_button.state(["!disabled"])
+    heading_label.config(text="Timer", fg=GREEN)
+    canvas.itemconfig(timer_text, text="00:00")
+    counter_label.config(text="")
+    global reps
+    reps = 0
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    start_button.state(["disabled"])
+
+    # If it's the 9th rep:
+    if reps == 9:
+        window.after_cancel(timer)
+        heading_label.config(text="Great job!", fg=GREEN)
+        canvas.itemconfig(timer_text, text="00:00")
+    # If it's the 8th rep:
+    elif reps % 8 == 0:
+        count_down(long_break_sec)
+        break_sound.play()
+        heading_label.config(text="Break", fg=RED)
+    # If it's 2nd/4th/6th rep:
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        break_sound.play()
+        heading_label.config(text="Break", fg=PINK)
+    # If it's the 1st/3rd/5th/7th rep:
+    else:
+        count_down(work_sec)  
+        work_sound.play()
+        heading_label.config(text="Work", fg=GREEN)
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer 
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+
+        for _ in range(work_sessions):
+            marks += "✔"
+            counter_label.config(text=marks)
+
+# ---------------------------- UI SETUP ------------------------------- #
+window.title("Tomato Timer")
+window.config(padx=60, pady=40, bg=YELLOW)
+window.iconphoto(False, icon_image)
+
+# ---------------------------- HEADING ------------------------------- #
+heading_label = Label(window, text="Timer", font=PACIFICO_FONT_LG, fg=GREEN, bg=YELLOW)
+heading_label.grid(column=1, row=0)
+
+# ---------------------------- TIMER ------------------------------- #
+canvas = Canvas(width=216, height=225, bg=YELLOW, highlightthickness=0)
+canvas.create_image(108, 110, image=tomato_img) # (x-axis position, y-axis position, image variable)
+timer_text = canvas.create_text(108, 110, text="00:00", fill=WHITE, font=PACIFICO_FONT_LG)
+canvas.grid(column=1, row=1)
+
+# ---------------------------- CONTROLS ------------------------------- #
+style = ttk.Style()
+style.configure("TButton", foreground=DARK_GREEN, background=GREEN, font=POPPINS_FONT)
+
+style.map("TButton",
+    background=[('pressed', DARK_GREEN), ('active', DARK_GREEN)]
+)
+
+start_button = ttk.Button(text="Start", takefocus=False, command=start_timer, width=10)
+start_button.grid(column=0, row=2)
+
+counter_label = Label(font=PACIFICO_FONT_SM, fg=GREEN, bg=YELLOW)
+counter_label.grid(column=1, row=3)
+
+reset_button = ttk.Button(text="Reset", takefocus=False, command=reset_timer, width=10)
+reset_button.grid(column=2, row=2)
+
+window.mainloop()
